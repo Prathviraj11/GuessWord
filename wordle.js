e8dfaf9d7c909b31ed661dc94e180cad6f58dbec
prra0922 <prathviraj200011@gmail.com>
2022-12-02 14:31:57 +0530

    Entering wrong words updated

--- wordle.js
+++ wordle.js
@@ -1,5 +1,7 @@
 let row = 0;
+let cursor = [];
 let absent = [];
+let correct = 0;
 let gameOver = false;
 let attempt = 10;
 
@@ -19,12 +21,20 @@ var guesslist = [
   "Dog",
 ];
 
-var word = guesslist[Math.floor(Math.random() * guesslist.length)].toUpperCase();
+var word =
+  guesslist[Math.floor(Math.random() * guesslist.length)].toUpperCase();
 console.log(word); // Random word generated from array guesslist
 
 //initialize function is called when page is loaded
 function intialize() {
-  document.getElementById("letterCount").innerHTML = "Letter count : " + word.length;
+  //Initialize Cursor
+  for (let i = 0; i < word.length; i++) {
+    cursor.push(i);
+  }
+  console.log("Cusor: " + cursor);
+
+  document.getElementById("letterCount").innerHTML =
+    "Letter count : " + word.length;
 
   for (let c = 0; c < word.length; c++) {
     let tile = document.createElement("span");
@@ -44,29 +54,28 @@ function processInput(e) {
   if (gameOver) return;
 
   if ("KeyA" <= e.code && e.code <= "KeyZ") {
-    if (row < word.length) {
-      let currTile = document.getElementById(row.toString());
+    if (cursor[row] < word.length) {
+      let currTile = document.getElementById(cursor[row].toString());
       if (currTile.innerHTML == "") {
         currTile.innerHTML = e.code[3];
       }
       row += 1;
     }
   } else if (e.code == "Backspace") {
-    if (0 < row && row <= 6) {
+    if (cursor[0] <= row && row <= cursor.length) {
       row -= 1;
+      let currTile = document.getElementById(cursor[row].toString());
+      currTile.innerText = "";
     }
-    let currTile = document.getElementById(row.toString());
-    currTile.innerText = "";
   } else if (e.code == "Enter") {
     update();
   }
 }
 
-
-//Validated user guessed word 
+//Validated user guessed word
 function update() {
   attempt -= 1;
-  let correct = 0;
+  cursor = [];
 
   for (let c = 0; c < word.length; c++) {
     let currTile = document.getElementById(c.toString());
@@ -76,14 +85,16 @@ function update() {
       currTile.classList.add("correct");
       correct += 1;
     } else if (!word.includes(letter)) {
-        if(!(absent.includes(letter)))
-            absent.push(letter);
-        currTile.innerHTML = "";
+      if (!absent.includes(letter)) {
+        absent.push(letter);
+      }
+      cursor.push(c);
+      currTile.innerHTML = "";
     } else {
       currTile.innerHTML = "";
+      cursor.push(c);
     }
 
-  
     document.getElementById("wrong").innerHTML = absent;
     document.getElementById("attempt").innerHTML = attempt;
 
@@ -93,12 +104,13 @@ function update() {
       gameOver = true;
     }
     if (attempt == 0 && correct != word.length) {
-      document.getElementById("message").innerHTML = "You Lost!! (Click reset to try again)";
+      document.getElementById("message").innerHTML =
+        "You Lost!! (Click reset to try again)";
       document.getElementById("body").style.backgroundColor = "red";
       gameOver = true;
     }
   }
-  console.log(correct);
-  row = 0; 
-
+  console.log("cusor:" + cursor);
+  row = 0;
+  correct = 0;
 }
